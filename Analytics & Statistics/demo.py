653af8da4ab329cfe310dbf8e0f3437ed7e1a2c5
anmol-pimpale <164032721+anmol-pimpale@users.noreply.github.com>
2024-08-18 16:23:15 +0530

    Add files via upload

---
+++ Analytics & Statistics/demo.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+
+iris = pd.read_csv("/home/hpcap/Desktop/Analytics & Statistics/iris.csv")
+exp_sals = pd.read_csv("/home/hpcap/Desktop/Analytics & Statistics/Exp_Salaries.csv")
+
+iris['Sepal.Length'].mean()
+iris['Sepal.Length'].median()
+
+a = np.array([45, 48, 56, 42, 35, 38, 40])
+print(a.mean())
+print(np.median(a))
+
+a = np.array([45, 48, 500, 42, 35, 38, 40])
+print(a.mean())
+print(np.median(a))
+
+exp_sals['Salary'].mean()
+males = exp_sals[exp_sals['Gender']=='Male']
+fem = exp_sals[exp_sals['Gender']=='Female']
+males['Salary'].mean()
+fem['Salary'].mean()
+# or
+exp_sals.groupby('Gender')['Salary'].mean()
+
+exp_sals.groupby('Department')['Salary'].mean()
+
+
+
